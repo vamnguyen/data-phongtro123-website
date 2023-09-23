cdd8268fa7f7cacee48c1a6164ff11d731757d98
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-09-23 15:04:18 +0700

    share data

--- index.js
+++
@@ -1,8 +0,0 @@
-const { startBrowser } = require("./browser");
-const scrapeController = require("./pageController");
-
-// Start the browser and create a browser instance
-let browser = startBrowser();
-
-// Pass the browser instance to the scraper controller
-scrapeController(browser);
